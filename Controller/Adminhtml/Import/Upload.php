72bcc45540b5b9868463fe9157552111222bc146
Perry <perry.mark@zeald.com>
2017-10-01 09:48:41 +0800

    Added validation for no file extension

--- Controller/Adminhtml/Import/Upload.php
+++ Controller/Adminhtml/Import/Upload.php
@@ -3,6 +3,7 @@
 namespace Prymag\ReviewsImporter\Controller\Adminhtml\Import;
 
 use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Controller\ResultFactory;
 
 class Upload extends \Magento\Backend\App\Action {
         /**
@@ -37,11 +38,34 @@ class Upload extends \Magento\Backend\App\Action {
              * fileId field must be the same name as the upload_field name from the form block
              * see Prymag\ReviewsImporter\Block\Adminhtml\Edit\Form
              */
-            $uploader = $this->uploaderFactory->create(['fileId' => 'reviews_import_file']);
-            $uploader->skipDbProcessing(true);
-            $result = $uploader->save($this->getWorkingDir());
+            try{
+                $uploader = $this->uploaderFactory->create(['fileId' => 'reviews_import_file']);
+                $uploader->checkAllowedExtension('csv');
+                $uploader->skipDbProcessing(true);
+                $result = $uploader->save($this->getWorkingDir());
+
+                $this->validateIfHasExtension($result);
+            }
+            catch( \Exception $e) {
+                $this->messageManager->addError( __( $e->getMessage() ) );
+                $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+                $resultRedirect->setPath('prymag_importer/import/index');
+                return $resultRedirect;
+            }
+
+
 
             return  $resultPage = $this->resultPageFactory->create();
+        }
+
+        public function validateIfHasExtension($result){
+            $extension = pathinfo($result['file'], PATHINFO_EXTENSION);
+            
+            $uploadedFile = $result['path'] . $result['file'];
+            if (!$extension) {
+                $this->varDirectory->delete($uploadedFile);
+                throw new \Exception(__('The file you uploaded has no extension.'));
+            }
         }
 
         public function getWorkingDir()
